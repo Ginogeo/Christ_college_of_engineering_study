9ba1d6ca7275cbc8790e77e8ef30ba5827cdf6a3
Ginogeo <ginogeo351@gmail.com>
2026-09-17 11:16:35 +0530

    .

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ model_path = Path(__file__).parent / "student_pass_fail_model (1).pkl"
 model = joblib.load(model_path)
 
 st.title("Student Pass Predictor")
-st.write("Enter the number of hours studied and Attendence to predict the result.")
+st.write("Enter the number of hours studied and Attendance to predict the result.")
 
 study_hours = st.number_input("Study hours", min_value=0.0, step=0.5)
-attendence = st.number_input("Atendence", min_value=0, step=1)
+attendance = st.number_input("Attendance", min_value=0, step=1)
 
 if st.button("Predict"):
-	input_data = pd.DataFrame({"StudyHours": [study_hours],"Attendence":[attendence]})
+	input_data = pd.DataFrame({"StudyHours": [study_hours],"Attendance":[attendance]})
 	prediction = model.predict(input_data)[0]
 	probability = model.predict_proba(input_data)[0][int(prediction)]
 
