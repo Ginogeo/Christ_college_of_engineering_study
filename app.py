d67a95ff8b02d6b97902dc483a6ceaf1d8a8ac86
Ginogeo <ginogeo351@gmail.com>
2026-09-18 09:04:17 +0530

    new model

--- app.py
+++ app.py
@@ -3,19 +3,18 @@ import pandas as pd
 import joblib
 from pathlib import Path
 
-model_path = Path(__file__).parent / "House_Price_Predictor(1).pkl"
+model_path = Path(__file__).parent / "poly_model_ac.pkl"
 model = joblib.load(model_path)
 
-st.title("House Price Predictor")
-st.write("Enter the Area , No. of Bedrromms and the No of Floors to predict the price")
+st.title("Price Prediction")
+st.write("Enter the details")
 
-area = st.number_input("Area", min_value=0.0, step=0.5)
-bedrooms = st.number_input("Bedrooms", min_value=0, step=1)
-floors = st.number_input("No. of Floors", min_value=0, step=1)
+ac_units= st.number_input("Area", min_value=0.0, step=0.5)
+# bedrooms = st.number_input("Bedrooms", min_value=0, step=1)
+# floors = st.number_input("No. of Floors", min_value=0, step=1)
 
-#[['Area_Sq_Ft',	'Total_Floors','Bedrooms']]
 if st.button("Predict"):
-	input_data = pd.DataFrame({"Area_Sq_Ft":[area],"Total_Floors":[floors],"Bedrooms":[bedrooms]})
+	input_data = pd.DataFrame({"AC_Units":[ac_units]})
 	prediction = model.predict(input_data)[0]
 	
 	if prediction:
